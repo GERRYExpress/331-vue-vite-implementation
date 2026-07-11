dfcafddcdf34e5df4ce6a2da139ba739089c1f97
GERRY Express <krittameth2020@gmail.com>
2026-07-11 09:34:17 +0700

    feat (8.4): add Event interface on types.ts, add array of Events

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,27 +1,16 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import type { Event } from '@/types.ts';
 
-/* defineProps<{
-  msg: string
-}>() */
+defineProps<{
+  event: Event
+}>();
 
-const event = ref({
-  id: 5928101,
-  category: 'animal welfare',
-  title: 'Cat Adoption Day',
-  description: 'Find your new feline friend at this event.',
-  location: 'Meow Town',
-  date: 'January 28, 2026',
-  time: '12:00',
-  petsAllowed: true,
-  organizer: 'Kat Laydee'
-})
 </script>
 
 <template>
   <div class="event-class">
     <div class="event-card">
-      <h2>{{ event.time }}</h2>
+      <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
     </div>
   </div>

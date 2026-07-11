2691b184edc33b722794be8a17c4670a1220e468
GERRY Express <krittameth2020@gmail.com>
2026-07-11 09:47:58 +0700

    feat (8.7): add organizer and category card in EventDetails.vue

---
+++ src/components/EventDetail.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { Event } from '@/types';
+defineProps<{
+    event: Event
+}>();
+</script>
+
+<template>
+    <div class="event-detail">
+        <p>Category: {{ event.category }}</p>
+        <p>Organizer: {{ event.organizer }}</p>
+    </div>
+</template>
+
+<style scoped>
+.event-detail {
+    display: flex;
+    flex-direction: column;
+    margin-left: auto;
+    text-align: right;
+    font-size: 20px;
+    margin: 12px 0;
+    border: 1px solid rebeccapurple;
+    padding: 20px;
+    max-width: 275px;
+    width: 100%;
+}
+</style>

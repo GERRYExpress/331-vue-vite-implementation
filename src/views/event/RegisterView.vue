17a307a6ebbe78ddea536b98ff14c0925cfabffc
GERRY Express <krittameth2020@gmail.com>
2026-07-18 10:33:40 +0700

    feat (4.2) vue-router: add register button

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import type { Event } from '@/types';
 import { toRefs } from 'vue';
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
 const props = defineProps<{
     event: Event
 }>()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { event } = toRefs(props)
+const register = () => {
+    router.push({ name: 'event-detail-view' })
+}
 </script>
 
 <template>
     <p>Register event here</p>
+    <button @click="register">Register</button>
 </template>

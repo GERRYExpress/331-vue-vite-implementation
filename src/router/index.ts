d12f23af9d8e047dc7c08b368f99fb999f4d90d9
GERRY Express <krittameth2020@gmail.com>
2026-07-18 10:45:37 +0700

    feat (5.2) vue-router: add network error handling

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import RegisterView from '@/views/event/RegisterView.vue'
 import EditView from '@/views/event/EditView.vue'
 import LayoutView from '@/views/event/LayoutView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import NetworkErrorView from '@/views/NetworkErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,6 +30,11 @@ const router = createRouter({
       name: '404-resource-view',
       component: NotFoundView,
       props: true,
+    },
+    {
+      path: '/network-error',
+      name: 'network-error-view',
+      component: NetworkErrorView,
     },
     {
       path: '/:catchAll(.*)',

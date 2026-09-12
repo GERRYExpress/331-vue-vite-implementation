1036b796c1322ac909acdc34ed9f2a3e1ba8f99f
GERRY Express <krittameth2020@gmail.com>
2026-09-12 13:55:08 +0700

    refactor(4.2): refactor organizer to match with backend DTO

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -12,6 +12,8 @@ defineProps<{
   <RouterLink class="decoration-0 text-[#2c3e50]" :to="{ name: 'event-detail-view', params: { id: event.id } }">
     <div class="hover:scale-101 hover:shadow-sp p-[20px] w-[250px] cursor-pointer border border-gray-600 mb-[18px]">
       <h2>{{ event.title }}</h2>
+      <span>by</span>
+      <h5>{{ event.organizer.name }}</h5>
       <span>@{{ event.time }} @ {{ event.location }}</span>
     </div>
     <EventDetail :event="event" />
--- src/types.ts
+++ src/types.ts
@@ -7,13 +7,12 @@ export interface Event {
   date: string
   time: string
   petsAllowed: boolean
-  organizer: string
+  organizer: Organizer
 }
 
 export interface Organizer {
   id: number | null
   name: string
-  address: string
 }
 
 export interface Student {
